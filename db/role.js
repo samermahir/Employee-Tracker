7d599e1022900abf562c69b1c9dda5a10d1fdfa6
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2022-12-26 11:35:51 -0800

    role/index

--- db/role.js
+++ db/role.js
@@ -1,3 +1,4 @@
+const { default: inquirer } = require("inquirer");
 const db = require("./connection");
 
 async function viewAllRoles() {
@@ -11,4 +12,44 @@ async function viewAllRoles() {
     }
 }
 
-module.exports = { viewAllRoles }
+async function addRole() {
+        try {
+        const roles= await viewAllRoles();
+        const {
+            title,
+            salary,
+            department_id
+            
+        } = await inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "What is the title or role of the employee?"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What is the salary of the employee?"
+            },
+            {
+                type: "list",
+                name: "department_id",
+                message: "What is the department of the employee?",
+                choices: roles.map(role => {
+                    return {
+                        value: role.id,
+                        name: role.department_id
+                    }
+                })
+            }
+        ]);  
+
+        await db.query(`INSERT into role (title, salary, department_id) VALUES (${title}, ${salary}, ${department_id})`)
+        const addRole = await viewAllRoles()
+        
+        return addRole;
+          } catch (err) {
+            console.log(err);
+          }
+        }
+    module.exports = { viewAllRoles, addRole, }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { prompt } = require("inquirer");
 const db = require("./db/connection");
 const { viewAllDepartments } = require('./db/departments');
 const { viewAllEmployees, addEmployee } = require('./db/employees');
-const { viewAllRoles } = require('./db/role');
+const { viewAllRoles, addRole } = require('./db/role');
 
 
 const start = async (s) => {
@@ -32,6 +32,7 @@ const start = async (s) => {
             const departments = await viewAllDepartments()
             console.table(departments)
             break;
+
         case 'View all employees':
             const employees = await viewAllEmployees()
             console.table(employees)
@@ -41,13 +42,17 @@ const start = async (s) => {
             const roles = await viewAllRoles()
             console.table(roles)
             break;
+
         case 'Add an employee':
             const newEmployees = await addEmployee()
             console.table(newEmployees)
             break;
-        // case 'Add a role':
-        //     const newRoles = await addRole()
-        //     console.table(newRoles)
+
+        case 'Add a role':
+            const newRoles = await addRole()
+            console.table(newRoles)
+            break;
+
         case 'Exit':
             console.log('Goodbye');
             process.exit();
